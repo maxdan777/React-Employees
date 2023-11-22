31933c20a2cfa3a0d4f4714f13931c9ab2057a76
Max Rytik <max777dan@gmail.com>
2023-11-22 21:22:14 +0300

    State lifting

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,9 +13,9 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                {name: 'Максим Рытик', salary: 45000, increase: false, id: 1},
-                {name: 'Бодров Кирилл', salary: 42000, increase: true, id: 2},
-                {name: 'Копотов Владимир', salary: 35000, increase: false, id: 3}
+                {name: 'Максим Рытик', salary: 45000, increase: true, like: true, id: 1},
+                {name: 'Бодров Кирилл', salary: 42000, increase: false, like: false, id: 2},
+                {name: 'Копотов Владимир', salary: 35000, increase: false, like: false, id: 3}
             ]
         }
         this.maxId = 4;
@@ -29,12 +29,12 @@ class App extends Component {
         })
     }
 
-    // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
     addItem = (name, salary) => {
         const newItem = {
             name, 
             salary,
             increase: false,
+            like: false,
             id: this.maxId++
         }
         this.setState(({data}) => {
@@ -45,10 +45,45 @@ class App extends Component {
         });
     }
 
+    onToggleProp = (id, prop) => {
+        // this.setState(({data}) => {
+        //     const index = data.findIndex(elem => elem.id ===id);
+
+        //     const old = data[index];
+        //     const newItem = {...old, increase: !old.increase};
+        //     const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+        //     return {
+        //         data: newArr
+        //     }
+        // })
+
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
+    // onToggleRise = (id) => {
+    //     this.setState(({data}) => ({
+    //         data: data.map(item => {
+    //             if (item.id === id) {
+    //                 return {...item, like: !item.like}
+    //             }
+    //             return item;
+    //         })
+    //     }))    }
+
     render() {
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
         return (
             <div className="app">
-                <AppInfo />
+                <AppInfo employees={employees} increased={increased}/>
     
                 <div className="search-panel">
                     <SearchPanel/>
@@ -57,7 +92,8 @@ class App extends Component {
                 
                 <EmployeesList 
                     data={this.state.data}
-                    onDelete={this.deleteItem}/>
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,32 +1,8 @@
-import { Component } from 'react';
-
 import './employees-list-item.css';
 
-class EmployeesListItem extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            increase: false,
-            like: false
-        }
-    }
-
-    onIncrease = () => {
-        this.setState(({increase}) => ({
-            increase: !increase
-        }))
-    }
-
-    onLike = () => {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
-
-    render () {
-        const {name, salary, onDelete} = this.props
-        const {increase} = this.state;
-        const {like} = this.state;
+const EmployeesListItem = (props) => {
+    
+        const {name, salary, onDelete, onToggleProp, increase, like} = props
 
         let classNames = "list-group-item d-flex justify-content-between";
         if (increase) {
@@ -39,12 +15,13 @@ class EmployeesListItem extends Component{
         return (
             <li className={classNames}>
                 <span className="list-group-item-label"
-                onClick={this.onLike}>{name}</span>
+                onClick={onToggleProp} data-toggle="like">{name}</span>
                 <input type="text" className="list-group-item-input" defaultValue={salary}/>
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
                         className="btn-cookie btn-sm "
-                        onClick={this.onIncrease}>
+                        onClick={onToggleProp}
+                        data-toggle="increase">
                         <i className="fas fa-cookie"></i>
                     </button>
     
@@ -57,7 +34,6 @@ class EmployeesListItem extends Component{
                 </div>
             </li>
         )
-    }
 }
 
 export default EmployeesListItem;
